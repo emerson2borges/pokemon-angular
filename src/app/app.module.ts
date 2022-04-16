c2fdecbfc3969fd5703a812bcc95fa8a064c660a
Emerson <emerson2borges@gmail.com>
2022-04-16 16:11:15 -0300

    feat(pokemon): instala ng-bootstrap. adiciona modal detalhes pokemon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
 import { PainelCardComponent } from './painel-card/painel-card.component';
+import { PainelModalComponent } from './painel-modal/painel-modal.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,11 @@ import { PainelCardComponent } from './painel-card/painel-card.component';
     TopoComponent,
     PainelComponent,
     PainelCardComponent,
+    PainelModalComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
